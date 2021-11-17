ebdae03f29ed0b82f9ce254dc30057d7433a63f8
Jalon Tuttle <jalontuttle@gmail.com>
2021-11-16 22:53:42 -0500

    adding updated README file

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -3,7 +3,7 @@ const userSeed = require('./userInfo');
 const postSeed = require('./postData');
 
 const seedData = async () => {
-    await sequelize.sync({ force: true })
+    await sequelize.sync({ force: true });
     await userSeed();
     await postSeed();
     console.log('Finished');
